0c3fd19aef07af4e9596292eabd814250123212e
AthanSteen <athanrony.vandensteen@leerling.go-atheneumoudenaarde.be>
2025-03-09 00:56:34 +0100

    made base command and updated inputmanager with xinput

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,25 +1,67 @@
 #include <SDL.h>
+#include <SDL_syswm.h>
+#include "Windows.h"
 #include "InputManager.h"
 #include "imgui.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_sdl2.h"
 
-bool dae::InputManager::ProcessInput()
-{
-	SDL_Event e;
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT) {
-			return false;
+namespace dae{
+	bool InputManager::ProcessInput()
+	{
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				return false;
+			}
+
+			if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
+				const auto KEY = e.key.keysym.sym;
+				const auto STATE = (e.type == SDL_KEYDOWN) ? InputState::Pressed : InputState::Up;
+				if (m_KeyboardCommands.find(KEY) != m_KeyboardCommands.end() &&
+					m_KeyboardCommands[KEY].find(STATE) != m_KeyboardCommands[KEY].end()) {
+					m_KeyboardCommands[KEY][STATE]->Execute();
+				}
+			}
+
+			//process event for IMGUI
+			ImGui_ImplSDL2_ProcessEvent(&e);
 		}
-		if (e.type == SDL_KEYDOWN) {
-			
+
+		XINPUT_STATE controllerState;
+		for (DWORD i = 0; i < XUSER_MAX_COUNT; ++i) {
+			ZeroMemory(&controllerState, sizeof(XINPUT_STATE));
+			if (XInputGetState(i, &controllerState) == ERROR_SUCCESS) {
+				// Controller is connected
+				for (const auto& [button, commands] : m_ControllerCommands)
+				{
+					bool isPressed = (controllerState.Gamepad.wButtons & button) != 0;
+					bool wasPressed = (controllerState.Gamepad.wButtons & button) != 0;
+
+					for (const auto& [commandState, command] : commands)
+					{
+						if ((commandState == InputState::Down && isPressed) ||
+							(commandState == InputState::Up && !isPressed && wasPressed) ||
+							(commandState == InputState::Pressed && isPressed && wasPressed))
+						{
+							command->Execute();
+						}
+					}
+				}
+				m_PreviousState = controllerState;
+			}
 		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			
-		}
-		//process event forr IMGUI
-		ImGui_ImplSDL2_ProcessEvent(&e);
+
+		return true;
 	}
 
-	return true;
+	void InputManager::BindKeyboardCommand(int key, InputState state, Command* command)
+	{
+		m_KeyboardCommands[key][state] = std::unique_ptr<Command>(command);
+	}
+
+	void InputManager::BindControllerCommand(WORD button, InputState state, Command* command)
+	{
+		m_ControllerCommands[button][state] = std::unique_ptr<Command>(command);
+	}
 }
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "Singleton.h"
+#include "Command.h"
+#include "InputState.h"
+#include "Xinput.h"
+#include <unordered_map>
+#include <memory>
 
 namespace dae
 {
@@ -7,6 +12,11 @@ namespace dae
 	{
 	public:
 		bool ProcessInput();
+		void BindKeyboardCommand(int key, InputState state, Command* command);
+		void BindControllerCommand(WORD button, InputState state, Command* command);
+    private:
+        std::unordered_map<int, std::unordered_map<InputState, std::unique_ptr<Command>>> m_KeyboardCommands;
+        std::unordered_map<WORD, std::unordered_map<InputState, std::unique_ptr<Command>>> m_ControllerCommands;
+		XINPUT_STATE m_PreviousState{};
 	};
-
 }
