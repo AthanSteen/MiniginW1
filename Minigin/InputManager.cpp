6340db0b542ee80a1d7ec8b439cad25199cd9c29
AthanSteen <athanrony.vandensteen@leerling.go-atheneumoudenaarde.be>
2025-03-15 15:47:46 +0100

    pimpl fixed

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,9 +1,49 @@
 #include <SDL.h>
 #include "InputManager.h"
 #include "backends/imgui_impl_sdl2.h"
+#include "Xinput.h"
 
 namespace dae{
+	class XInputImpl {
+	public:
+		XInputImpl() = default;
+
+		bool ProcessInput() {
+			XINPUT_STATE controllerState;
+			for (DWORD i = 0; i < XUSER_MAX_COUNT; ++i) {
+				ZeroMemory(&controllerState, sizeof(XINPUT_STATE));
+				if (XInputGetState(i, &controllerState) == ERROR_SUCCESS) {
+					// Controller is connected
+					for (const auto& [button, commands] : m_ControllerCommands) {
+						bool isPressed = (controllerState.Gamepad.wButtons & button) != 0;
+						bool wasPressed = (m_previousState.Gamepad.wButtons & button) != 0;
+
+						for (const auto& [commandState, command] : commands) {
+							if ((commandState == InputState::Down && isPressed && !wasPressed) ||
+								(commandState == InputState::Up && !isPressed && wasPressed) ||
+								(commandState == InputState::Pressed && isPressed && wasPressed)) {
+								command->Execute();
+							}
+						}
+					}
+					m_previousState = controllerState;
+				}
+			}
+			return true;
+		}
+
+		void BindControllerCommand(WORD button, InputState state, Command* command) {
+			m_ControllerCommands[button][state] = std::unique_ptr<Command>(command);
+		}
+
+	private:
+		std::unordered_map<WORD, std::unordered_map<InputState, std::unique_ptr<Command>>> m_ControllerCommands;
+		XINPUT_STATE m_previousState{};
+	};
+
+
 	InputManager::InputManager() : m_XInputImpl(std::make_unique<XInputImpl>()) {}
+	InputManager::~InputManager() = default;
 
 	bool InputManager::ProcessInput()
 	{
@@ -28,8 +68,7 @@ namespace dae{
 				}
 			}
 		}
-		m_XInputImpl->ProcessControllerInput(m_ControllerCommands);
-
+		m_XInputImpl->ProcessInput();
 		return true;
 	}
 
@@ -40,6 +79,6 @@ namespace dae{
 
 	void InputManager::BindControllerCommand(WORD button, InputState state, Command* command)
 	{
-		m_ControllerCommands[button][state] = std::unique_ptr<Command>(command);
+		m_XInputImpl.get()->BindControllerCommand(button, state, command);
 	}
 }
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -3,22 +3,22 @@
 #include "Singleton.h"
 #include "Command.h"
 #include "InputState.h"
-#include "XInputImpl.h"
 #include <unordered_map>
 #include <memory>
 
 namespace dae
 {
+	class XInputImpl;
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
 		InputManager();
+		~InputManager();
 		bool ProcessInput();
 		void BindKeyboardCommand(int key, InputState state, Command* command);
 		void BindControllerCommand(WORD button, InputState state, Command* command);
     private:
         std::unordered_map<int, std::unordered_map<InputState, std::unique_ptr<Command>>> m_KeyboardCommands;
-        std::unordered_map<WORD, std::unordered_map<InputState, std::unique_ptr<Command>>> m_ControllerCommands;
-		std::unique_ptr<class XInputImpl> m_XInputImpl;
+		std::unique_ptr<XInputImpl> m_XInputImpl;
 	};
 }
